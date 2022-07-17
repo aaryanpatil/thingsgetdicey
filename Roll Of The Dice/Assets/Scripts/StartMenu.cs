9ca529a22ab40a5660980294e76a413ae067420a
aaryanpatil <aaryanpatil.work@gmail.com>
2022-07-17 22:53:14 +0530

    Start Menu Transitions

--- Roll Of The Dice/Assets/Scripts/StartMenu.cs
+++ Roll Of The Dice/Assets/Scripts/StartMenu.cs
@@ -1,16 +1,25 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
+    [SerializeField] float loadDelay = 2.5f;
     Animator animator;
 
     private void Awake() 
     {
-        animator = GetComponent<Animator>();    
+        FindObjectOfType<SceneTransition>();    
     }
     public void LoadStartLevel()
     {
+        FindObjectOfType<SceneTransition>().MakeTransition();
+        StartCoroutine(LoadLevel1());
+    }
+
+    IEnumerator LoadLevel1()
+    {
+        yield return new WaitForSecondsRealtime(loadDelay);
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex + 1);
     }
